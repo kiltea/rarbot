567bd616da5805a4ee331bd59fec67b8d2077301
rardooba <rardooba@gmail.com>
2024-01-10 14:58:54 +0100

    test 🦄

--- index.js
+++ index.js
@@ -1,58 +1,27 @@
+
 const jsonfile = require("jsonfile");
 const moment = require("moment");
 const simpleGit = require("simple-git");
-// const random = require("random");
 
-const FILE_PATH = "./data.json";
+import('random').then((module) => {
+  random = module.default;
 
-const DATE = moment().subtract(6, 'd').format();
+  const FILE_PATH = './data.json';
 
-const data = {
-  date: DATE,
-};
-
-jsonfile.writeFile(FILE_PATH, data);
-
-simpleGit()
-  .add([FILE_PATH])
-  .commit(
-    DATE,
-    {
-      "--date": DATE,
-    }
-  ).push();
-
-// const createCommit = (numberOfCommit) => {
-//   if (numberOfCommit === 0) return simpleGit().push();
-
-//   const weeks = random.int(0, 54);
-//   const days = random.int(0, 6);
-
-//   const DATE = moment()
-//     .subtract(1, "y")
-//     .add(1, "d")
-//     .add(weeks, "w")
-//     .add(days, "d")
-//     .format();
-
-//   const data = {
-//     date: DATE,
-//   };
-
-//   jsonfile.writeFile(FILE_PATH, data, () => {
-//     simpleGit()
-//       .add([FILE_PATH])
-//       .commit(
-//         DATE,
-//         {
-//           "--date": DATE,
-//         },
-//         () => {
-//           createCommit(numberOfCommit - 1);
-//           simpleGit().push();
-//         }
-//       );
-//   });
-// };
-
-// createCommit(500);
+  const makeCommit = n => {
+      if (n === 0) return simpleGit().push();
+      const x = random.int(0, 54);
+      const y = random.int(0, 6);
+      const DATE = moment().subtract(3, 'y').add(1, 'd')
+                      .add(x, 'w').add(y, 'd').format();
+      const data = {
+          date: DATE
+      }
+      console.log(DATE);
+      jsonfile.writeFile(FILE_PATH, data, () => {
+          simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }, 
+          makeCommit.bind(this, --n));
+      });
+  }
+  makeCommit(500);
+});
