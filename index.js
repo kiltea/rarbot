aac190469f4eaaf91bb6601a920269183667cdb6
rardooba <rardooba@gmail.com>
2024-01-17 12:33:22 +0100

    😱 test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import('random').then((module) => {
       if (n === 0) return simpleGit().push();
       const x = random.int(0, 54);
       const y = random.int(0, 6);
-      const DATE = moment().subtract(3, 'y').add(1, 'd')
+      const DATE = moment().subtract(1, 'y').add(1, 'd')
                       .add(x, 'w').add(y, 'd').format();
       const data = {
           date: DATE
